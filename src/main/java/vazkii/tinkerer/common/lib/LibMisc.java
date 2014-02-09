afa307c1498b029bcdc3fcb297a808b3a8f1ac7d
nekosune <kat.swales@nekokittygames.com>
2014-02-09 22:34:24 +0000

    Fixed Boots

--- src/main/java/vazkii/tinkerer/common/lib/LibMisc.java
+++ src/main/java/vazkii/tinkerer/common/lib/LibMisc.java
@@ -18,7 +18,7 @@ public final class LibMisc {
 
 	public static final String MOD_ID = "ThaumicTinkerer";
 	public static final String MOD_NAME = "Thaumic Tinkerer";
-	public static final String VERSION = "2.2b-82FC";
+	public static final String VERSION = "2.2b-83FC";
 
 	public static final String NETWORK_CHANNEL = MOD_ID;
 
